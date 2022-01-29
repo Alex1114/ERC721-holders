9a5852c0da78190e440f91f0f9e17a0480decc7f
alex1114 <pokai.eed05@g2.nctu.edu.tw>
2022-01-29 17:08:39 +0800

    0129

---
+++ app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const Web3 = require('web3');
+const config = require('./config/params');
+const utils = require('./src/utils');
+
+const web3 = new Web3(new Web3.providers.HttpProvider(config.HTTP_PROVIDER));
+const contract = new web3.eth.Contract(config.ABI_ARRAY, config.CONTRACT_ADDRESS);
+
+(async () => {
+    // await utils.clearFile(config.FILE_NAME);
+    await contract.methods.totalSupply().call()
+        .then(async (total) => {
+            const holders = [];
+            for (let i = 26000; i < 30008; i += 1) {
+                utils.addHolder(holders, contract, i);
+            
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})();
---
+++ src/utils.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const config = require('../config/params');
+
+async function addHolder(holders, contract, tokenId) {
+    await contract.methods.ownerOf(tokenId).call()
+        .then(async (holder) => {
+            if (holders.indexOf(holder) === -1) {
+                await fs.appendFile(config.FILE_NAME, `${holder.toString()}\n`, (err) => {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    return true;
+                });
+                holders.push(holder);
+                console.log(tokenId);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+async function clearFile(path) {
+    if (await fs.existsSync(path)) {
+        await fs.unlink(path, (err) => {
+            if (err) {
+                return console.log(err);
+            }
+            return true;
+        });
+    }
+    return true;
+}
+
+module.exports = {
+    addHolder,
+    clearFile,
+};
